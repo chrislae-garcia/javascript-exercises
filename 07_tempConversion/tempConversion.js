95077f06bb3ba3a385b9ff8bdc9e3da1a32e1f35
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-04 18:48:29 +0800

    Write ctof() functionality

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,8 +4,10 @@ const ftoc = function(f) {
   return parseFloat(c.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  let f = (1.8 * c) + 32;
 
+  return parseFloat(f.toFixed(1));
 };
 
 // Do not edit below this line
