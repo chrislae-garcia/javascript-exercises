780fb646c3e0895c57e99f53ceaec4c47ad7cfe9
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-04 14:07:43 +0800

    Write reverseString() functionality

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversed = '';
 
+  for (let i = 0; i < string.length; i++){
+    reversed = string.charAt(i) + reversed;
+  }
+  return reversed;
 };
 
 // Do not edit below this line
