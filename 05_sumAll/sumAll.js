875ba0a42c65ca20fcdaf0108908026c590a9129
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-04 17:11:38 +0800

    Write sumAll() functionality

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function (low, high) {
+  let temp;
+
+  let sum = 0;
+
+  if (low < 0 || high < 0) {
+    
+    return 'ERROR';
+
+  } else if (typeof (low) != 'number' || typeof (high) != 'number') {
+    
+    return 'ERROR';
+
+  } else {
+
+    if (low > high) {
+      temp = low;
+      low = high;
+      high = temp;
+    }
+
+    for (i = low; i <= high; i++) {
+      sum = sum + i;
+    }
+
+    return sum;
+  }
 
 };
 
