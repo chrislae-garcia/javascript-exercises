4dc9e560995f4ebdba9b799b807edd48e60d89ad
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-12 02:14:41 +0800

    Add functionality to fibonacci.js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = number => {
 
+  if (number < 0) return "OOPS";
+
+  let array = [];
+
+  index = parseInt(number) - 1;
+
+  for (i = 0; i <= index; i++) {
+    if (i <= 1) 
+      array.push(1);
+    else 
+      array.push(array.at(-2) + array.at(-1));
+  }
+
+  return array[index];
 };
 
 // Do not edit below this line
