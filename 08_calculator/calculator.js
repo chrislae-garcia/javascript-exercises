aefb8244245a0cc74f58a1cdea692dee2d28df02
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-11 22:29:30 +0800

    Fill out functions to satisfy test cases

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = (numbers) => { 
+  let sum = 0;
+  for (let i = 0; i < [...numbers].length; i++) {
+    sum += parseInt(numbers[i]);
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const multiply = (numbers) => {
+  let product = 1;
+  for (let i = 0; i < [...numbers].length; i++) {
+    product *= parseInt(numbers[i]);
+  }
+  return product;
 };
 
-const sum = function() {
-	
-};
+const power = (base, exponent) => base**exponent;
 
-const multiply = function() {
+const factorial = (number) => {
+	if (number < 0) return undefined;
+  if (number == 0) return 1;
 
-};
+  let n = number-1;
 
-const power = function() {
-	
-};
+  while(n > 1) {
+    number*=n;
+    n--;
+  }
 
-const factorial = function() {
-	
+  return number;
 };
 
 // Do not edit below this line
