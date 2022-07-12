d662240d0b3667541d5481e2ac8999eac7ea40f5
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-12 19:39:00 +0800

    Add functionality to findTheOldest.js exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = people => {
+
+  people.sort((a,b) => {
+    let currentYear = new Date().getFullYear();
+    
+    let aAge, bAge;
+
+    if (!a.yearOfDeath) {
+      aAge = currentYear - a.yearOfBirth
+    } else {
+      aAge = a.yearOfDeath - a.yearOfBirth
+    }
+    
+    if (!b.yearOfDeath) {
+      bAge = currentYear - b.yearOfBirth;
+    } else {
+      bAge = b.yearOfDeath - b.yearOfBirth;
+    }
+
+    return aAge > bAge ? -1 : 1;
+  });
+  
+  return people[0];
 
 };
 
